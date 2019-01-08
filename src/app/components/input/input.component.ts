344d37e3ce8ce1d19b98c644d9fce6ad70334a14
Harry <hbishop11@gmail.com>
2019-01-08 20:28:51 +0100

    Made a quick start on my own input :P

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
+import { Component, OnInit, HostListener, EventEmitter, Input, Output } from '@angular/core';
 import { Observable } from 'rxjs';
+
+import * from 'lodash';
 
 @Component({
     selector: 'pan-input',
@@ -10,7 +12,16 @@ import { Observable } from 'rxjs';
 })
 export class InputComponent implements OnInit {
     public hints: any[] = [];
-    public input: string;
+    public input = '';
+
+    private caretPosition = 0;
+
+    @HostListener('window:keydown', ['$event'])
+    handleKeyDown(event: KeyboardEvent) {
+        event.preventDefault();
+        this.handleKeypress(event);
+      // event.key === 'ArrowUp'
+    }
 
     constructor() {}
 
@@ -89,11 +100,79 @@ export class InputComponent implements OnInit {
         });
     }
 
-    public onInputChange(event: any) {
-        event.preventDefault();
-        console.log(event);
-        // so we need to alter the options shown to reduce based on what's available
+    private handleKeypress(keyboardEvent: KeyboardEvent) {
+        // if user presses delete
+        if (keyboardEvent.key === 'Backspace') {
+            this.handleBackspace();
+            return;
+        }
 
-        // then when its been chosen, 
+        // if user presses enter
+        if (keyboardEvent.key === 'Enter') {
+            // this.handleEnter(event);
+            return;
+        }
+
+        // if user presses tab
+        if (keyboardEvent.key === 'Tab') {
+            this.completeSuggestion();
+            return;
+        }
+
+        // if user presses left
+        if (keyboardEvent.key === 'ArrowLeft') {
+            return;
+        }
+
+        // if user presses right
+        if (keyboardEvent.key === 'ArrowRight') {
+            return;
+        }
+
+        // if user presses other special character
+        if (keyboardEvent.key === 'Shift' || keyboardEvent.key === 'Meta' || keyboardEvent.key === 'Control') {
+            return;
+        }
+
+        // otherwise
+        this.handleInput(keyboardEvent);
+    }
+
+    private handleBackspace() {
+        this.input = this.input.slice(0, this.caretPosition - 1);
+        this.caretPosition--;
+    }
+
+    private handleInput(keyboardEvent: KeyboardEvent) {
+        this.input += event.key;
+        this.caretPosition++;
+
+        this.suggestion = this.getSuggestion(this.input);
+    }
+
+    private getSuggestion(input: string) {
+        const hint = _.find(this.hints, function(hint) {
+            return _.startsWith(input, hint.option);
+        });
+
+        if (hint) {
+            // TODO = implement this bit!
+        } else {
+            const suggestion = _.find(this.hints, function(hint) {
+                return _.startsWith(hint.option, input);
+            });
+        }
+
+        if (!suggestion) {
+            return '';
+        }
+
+        return _.replace(suggestion.option, input, '');
+    }
+
+    private completeSuggestion() {
+        this.input += this.suggestion;
+        this.suggestion = '';
+        this.caretPosition = this.input.length;
     }
 }
