5aca8cb2430b4d8b4f810fc2ec81677cf9f70405
Harry <hbishop11@gmail.com>
2019-06-10 23:04:27 +0200

    Reimplemented the auto-show messages

--- src/app/components/feed/feed.component.ts
+++ src/app/components/feed/feed.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, HostListener, Input, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import * as _ from 'lodash';
@@ -19,43 +19,22 @@ export class FeedComponent implements OnInit {
     public messages: Message[] = [];
     public hasUnreadMessages = false;
 
-    private keysMap = {};
-
-    @HostListener('window:keydown', ['$event'])
-    handleKeyDown(event: KeyboardEvent) {
-        this.handleKeypress(event);
-    }
-
-    @HostListener('window:keyup', ['$event'])
-    handleKeyUp(event: KeyboardEvent) {
-        this.handleKeypress(event);
-    }
-
     constructor() {}
 
     public ngOnInit() {
         this.feed
             .messageStream
             .subscribe((message) => {
+                console.log('message received!', message);
                 if (message.isClear) {
                     this.messages = [];
                     this.hasUnreadMessages = false;
                 } else {
                     this.messages.push(message);
                     this.hasUnreadMessages = true;
+                    this.showNextMessage();
                 }
             });
-    }
-
-    private handleKeypress(event) {
-        this.keysMap[event.key] = event.type === 'keydown';
-
-        // Cmd + m or Ctrl + m
-        if (this.keysMap['Meta'] && this.keysMap['m'] || this.keysMap['Control'] && this.keysMap['m']) {
-            this.keysMap['m'] = false;
-
-            this.showNextMessage();
-        }
     }
 
     private showNextMessage() {
--- src/app/models/feed.model.ts
+++ src/app/models/feed.model.ts
@@ -1,17 +1,67 @@
-import { Subject } from 'rxjs';
+import { Subject, Observable, pipe, zip, from, interval, of } from 'rxjs';
+import { map, concatMap, delay, pairwise } from 'rxjs/operators';
 
 import { Message } from './message.model';
 
 export class Feed {
     public messageStream: Subject<Message>;
+    private messageQueue: Subject<Message>;
+
+    private observer: any;
+    private messageQueueObserver: any;
+    private messageSentObserver: any;
+
+    private delay: number;
+
+    private isFirstMessage = true;
+    private WORDS_PER_MILLISECOND: number = 300 / 60 / 1000;
+    private MAX_WAIT = 3000;
 
     constructor() {
+        this.messageQueue = new Subject();
+
         this.messageStream = new Subject();
+
+        this.messageQueue
+            .pipe(
+                pairwise(),
+                concatMap(messages => {
+                    console.log('doing something');
+                    const lastMessage = messages[0];
+                    const currentMessage = messages[1];
+
+                    const _delay = this.calculateTimeout(lastMessage);
+
+                    return of(currentMessage)
+                        .pipe(
+                            delay(_delay)
+                        );
+                }),
+            )
+            .subscribe(message => {
+                this.messageStream.next(message);
+            });
     }
 
     public addMessage(message: Message): Feed {
-        this.messageStream.next(message);
+        this.messageQueue.next(message);
 
         return this;
+    }
+
+    private calculateTimeout(message: Message): number {
+        if (message.text) {
+            const numberOfWords = message.text.split(' ').length;
+            const waitTime = numberOfWords / this.WORDS_PER_MILLISECOND;
+
+            if (waitTime > this.MAX_WAIT) {
+                return this.MAX_WAIT;
+            } else {
+                return waitTime;
+            }
+        }
+
+
+        return 0;
     }
 }
