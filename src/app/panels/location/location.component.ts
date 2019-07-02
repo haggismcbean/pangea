006d7d590b828ba406dad7aa89d5345df51ba740
Harry <hbishop11@gmail.com>
2019-07-02 20:13:30 +0200

    Using syntax highlighter plugin to enable some pretty weather

--- src/app/panels/location/location.component.ts
+++ src/app/panels/location/location.component.ts
@@ -2,13 +2,29 @@ import { Component } from '@angular/core';
 
 import { CLOUDY, FOGGY, HEAVY_RAIN } from './constants/weather';
 
+import hljs from 'highlight.js/lib/highlight';
+
 @Component({
     selector: 'pan-location',
     templateUrl: './location.component.html',
     styleUrls: ['./location.component.scss']
 })
 export class LocationComponent {
+
+    public weatherGlyph;
+
     constructor() {
-        this.weatherGlyph = FOGGY;
+        const weatherHighlighting = {
+            case_insensitive: true,
+            keywords: {
+                grey: '- _',
+                blue: 'ʻ ‚',
+            },
+            contains: []
+        };
+
+        hljs.registerLanguage('weather', () => weatherHighlighting);
+        this.weatherGlyph = hljs.highlight('weather', FOGGY);
+        console.log(this.weatherGlyph);
     }
 }
